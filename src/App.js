a93324bf1ed7d75d22975ea941e28149b4d183ff
YujieLyu <jessie.yujie.lyu@gmail.com>
2020-08-22 17:53:41 +1000

    components-init/ProjectDetails component and setup route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard'
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails'
+import ProjectList from './components/projects/ProjectList';
 
 class App extends Component {
   render() {
@@ -10,7 +12,8 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
           </Switch>
         </div>
       </BrowserRouter>
